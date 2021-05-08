2394a844ab723e59330f0e55d3d78cec5554ab6d
Aaron Winter <winter.aaron93@gmail.com>
2021-05-08 16:38:47 +0200

    edit terrain

--- src/editor/EditorEditSystem.cs
+++ src/editor/EditorEditSystem.cs
@@ -68,10 +68,11 @@ public class EditorEditSystem : IEcsInitSystem, IEcsRunSystem
     private void EditLocation(EcsEntity editorEntity, EcsEntity locEntity)
     {
         ref var editorView = ref editorEntity.Get<NodeHandle<EditorView>>().Node;
-
+        ref var terrain = ref locEntity.Get<Terrain>();
         ref var elevation = ref locEntity.Get<Elevation>();
 
         elevation.Level = editorView.Elevation;
+        terrain = editorView.Terrain;
     }
 
     private void SendUpdateMapEvent()
--- src/editor/EditorView.cs
+++ src/editor/EditorView.cs
@@ -6,12 +6,42 @@ public partial class EditorView : CanvasLayer
     public int BrushSize { get { return (int) _brushSizeSlider.Value; } }
     public int Elevation { get { return (int) _elevationSlider.Value; } }
 
+    public Terrain Terrain { get { return _selectedTerrain; } }
+
+    Terrain _selectedTerrain;
+
     HSlider _brushSizeSlider;
     HSlider _elevationSlider;
 
+    Control _terrains;
+
     public override void _Ready()
-    {
+    {   
+        _terrains = GetNode<Control>("Terrains/VBoxContainer");
+
         _elevationSlider = GetNode<HSlider>("PanelContainer/VBoxContainer/Elevation/HSlider");
         _brushSizeSlider = GetNode<HSlider>("PanelContainer/VBoxContainer/BrushSize/HSlider");
+
+        InitializeTerrains();
+    }
+
+    public void InitializeTerrains()
+    {   
+        _selectedTerrain = Data.Instance.Terrains["Gg"];
+
+        foreach (var item in Data.Instance.Terrains)
+        {
+            var code = item.Key;
+
+            var button = new Button();
+            button.Text = code;
+            button.Connect("pressed", new Callable(this, "OnTerrainSelected"), new Godot.Collections.Array() { code });
+            _terrains.AddChild(button);
+        }
+    }
+
+    public void OnTerrainSelected(string code)
+    {
+        _selectedTerrain = Data.Instance.Terrains[code];
     }
 }
