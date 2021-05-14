8e911fd2c5d440d6c768ce96a676ffa88c265a49
Aaron Winter <winter.aaron93@gmail.com>
2021-05-14 09:34:13 +0200

    two types of dummy units

--- src/unit/UnitView.cs
+++ src/unit/UnitView.cs
@@ -3,5 +3,18 @@ using System;
 
 public partial class UnitView : Node3D
 {
-    
+
+    public override void _Ready()
+    {
+        if (GD.Randf() < 0.5)
+        {
+            GetNode<Node3D>("Infantry").Visible = true;
+            GetNode<Node3D>("Unit").Visible = false;
+        }
+        else
+        {
+            GetNode<Node3D>("Infantry").Visible = false;
+            GetNode<Node3D>("Unit").Visible = true;
+        }
+    }
 }
