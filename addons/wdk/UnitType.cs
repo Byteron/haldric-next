aee3b257febf530a4d484f24103e1f5560105eba
Aaron Winter <winter.aaron93@gmail.com>
2021-10-24 10:31:19 +0200

    remove defenses from units

--- addons/wdk/UnitType.cs
+++ addons/wdk/UnitType.cs
@@ -6,14 +6,12 @@ namespace Haldric.Wdk
     public partial class UnitType : Node3D
     {
         public Attributes Attributes;
-        public Defenses Defenses;
         public UnitView UnitView;
         public Node Attacks;
 
         public override void _Ready()
         {
             Attributes = GetNode<Attributes>("Attributes");
-            Defenses = GetNode<Defenses>("Defenses");
             UnitView = GetNode<UnitView>("UnitView");
             Attacks = GetNode<Node>("Attacks");
         }
