866abfb4339b4c6fab60564d6f779393e08adcb0
Aaron Winter <winter.aaron93@gmail.com>
2021-11-01 18:54:57 +0100

    sky / light energy

--- src/systems/scenario/TurnEndEventSystem.cs
+++ src/systems/scenario/TurnEndEventSystem.cs
@@ -73,7 +73,7 @@ public class TurnEndEventSystem : IEcsSystem
                 {
                     canHeal = canHeal || locEntity.Get<HasOverlayTerrain>().Entity.Has<Heals>();
                 }
-                
+
                 var unitEntity = locWithUnitQuery.Get<HasUnit>(locEntityId).Entity;
 
                 ref var health = ref unitEntity.Get<Attribute<Health>>();
@@ -103,16 +103,16 @@ public class TurnEndEventSystem : IEcsSystem
 
         tween.SetTrans(Tween.TransitionType.Sine);
         tween.SetEase(Tween.EaseType.InOut);
-        
-        tween.Parallel().TweenProperty(Main.Instance.Light, "rotation", new Vector3(Mathf.Deg2Rad(daytime.Angle), 0, 0), 2.5f);
 
+        tween.Parallel().TweenProperty(Main.Instance.Light, "rotation", new Vector3(Mathf.Deg2Rad(daytime.Angle), 0, 0), 2.5f);
         tween.Parallel().TweenProperty(Main.Instance.Light, "light_color", daytime.LightColor, 2.5f);
-        tween.Parallel().TweenProperty(Main.Instance.Light, "light_energy", daytime.Energy, 2.5f);
-        
+        tween.Parallel().TweenProperty(Main.Instance.Light, "light_energy", daytime.LightEnergy, 2.5f);
+
+        tween.Parallel().TweenProperty(Main.Instance.Environment.Environment.Sky.SkyMaterial, "sky_energy", daytime.SkyEnergy, 2.5f);
         tween.Parallel().TweenProperty(Main.Instance.Environment.Environment.Sky.SkyMaterial, "sky_top_color", daytime.SkyColor, 2.5f);
         tween.Parallel().TweenProperty(Main.Instance.Environment.Environment.Sky.SkyMaterial, "sky_horizon_color", daytime.SkyColor, 2.5f);
         tween.Parallel().TweenProperty(Main.Instance.Environment.Environment.Sky.SkyMaterial, "ground_horizon_color", daytime.SkyColor, 2.5f);
-        
+
         tween.Play();
     }
 }
