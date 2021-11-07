d7b7ce99615059da69bcc3e8900bc085d2adb9cb
Zozer Firehood <ydo@verizon.net>
2021-11-07 15:23:36 -0500

    basis of bordered region

--- src/nodes/hud/TerrainHighlighter.cs
+++ src/nodes/hud/TerrainHighlighter.cs
@@ -3,53 +3,62 @@ using System.Collections.Generic;
 
 public partial class TerrainHighlighter : Node3D
 {
-    [Export] public PackedScene TerrainHighlight;
-    public List<TerrainHighlight> _path = new List<TerrainHighlight>();
+	[Export] public PackedScene TerrainHighlight;
+	public List<Border3D> Path { get; set; } = new List<Border3D>();
 
-    public void ShowPath(Path path)
-    {
-        foreach (var child in _path)
-        {
-            RemoveChild(child);
-            child.QueueFree();
-        }
+	public void ShowPath(Path path)
+	{
+		foreach (var child in Path)
+		{
+			RemoveChild(child);
+			child.QueueFree();
+		}
 
-        _path.Clear();
+		Path.Clear();
 
-        foreach(var pLocEntity in path.Checkpoints)
-        {
-            ref var coords = ref pLocEntity.Get<Coords>();
-            ref var elevation = ref pLocEntity.Get<Elevation>();
+		foreach(var pLocEntity in path.Checkpoints)
+		{
+			ref var coords = ref pLocEntity.Get<Coords>();
+			ref var elevation = ref pLocEntity.Get<Elevation>();
 
-            var pos = coords.World;
-            pos.y = elevation.Height;
+			var pos = coords.World;
+			pos.y = elevation.Height;
 
-            PlaceHighlight(pos, new Color("FFFFFF"), 0.7f, true);
-        }
-    }
+			//PlaceHighlight(pos, new Color("FFFFFF"), 0.7f, true);
+		}
+	}
 
-    public void PlaceHighlight(Vector3 position, Color color, float scaleFactor, bool isPath = false)
-    {
-        var highlight = TerrainHighlight.Instantiate<TerrainHighlight>();
-        highlight.Color = color;
-        highlight.ScaleFactor = scaleFactor;
-        highlight.Position = position;
-        AddChild(highlight);
+	public void PlaceHighlight(Vector3 position, Color color, float scaleFactor, bool isPath = false)
+	{
+		var highlight = TerrainHighlight.Instantiate<Border3D>();
+		highlight.Color = color;
+		//highlight.ScaleFactor = scaleFactor;
+		highlight.Position = position;
+		AddChild(highlight);
 
-        if (isPath)
-        {
-            _path.Add(highlight);
-        }
-    }
+		if (isPath)
+		{
+			Path.Add(highlight);
+		}
+	}
 
-    public void Clear()
-    {
-        foreach (Node child in GetChildren())
-        {
-            RemoveChild(child);
-            child.QueueFree();
-        }
+	public void PlaceBorder(Vector3 position, Color color, float rotate)
+	{
+		Border3D highlight = TerrainHighlight.Instantiate<Border3D>();
+		highlight.Color = color;
+		highlight.Direction = rotate;
+		highlight.Position = position;
+		AddChild(highlight);
+	}
 
-        _path.Clear();
-    }
+	public void Clear()
+	{
+		foreach (Node child in GetChildren())
+		{
+			RemoveChild(child);
+			child.QueueFree();
+		}
+
+		Path.Clear();
+	}
 }
--- src/systems/map/HighlightLocationEventSystem.cs
+++ src/systems/map/HighlightLocationEventSystem.cs
@@ -1,123 +1,148 @@
 using Bitron.Ecs;
 using Godot;
-
+using System.Collections.Generic;
 public struct HighlightLocationEvent
 {
-    public Coords Coords { get; set; }
-    public int Range { get; set; }
+	public Coords Coords { get; set; }
+	public int Range { get; set; }
 
-    public HighlightLocationEvent(Coords coords, int range)
-    {
-        Coords = coords;
-        Range = range;
-    }
+	public HighlightLocationEvent(Coords coords, int range)
+	{
+		Coords = coords;
+		Range = range;
+	}
 }
 
 public class HighlightLocationsEventSystem : IEcsSystem
 {
-    public void Run(EcsWorld world)
-    {
-        var query = world.Query<HighlightLocationEvent>().End();
+	private Dictionary<Direction, float> _rotations = new Dictionary<Direction, float>() {
+		{ Direction.W, 0 },
+		{ Direction.NW, Godot.Mathf.Pi/3 },
+		{ Direction.NE, 2 * Godot.Mathf.Pi/3 },
+		{ Direction.E, Godot.Mathf.Pi },
+		{ Direction.SE, - Godot.Mathf.Pi/3 },
+		{ Direction.SW, - 2 * Godot.Mathf.Pi/3 }
+	};
+	public void Run(EcsWorld world)
+	{
+		var query = world.Query<HighlightLocationEvent>().End();
 
-        foreach (var eventEntityId in query)
-        {
-            var map = world.GetResource<Map>();
-            var grid = map.Grid;
+		foreach (var eventEntityId in query)
+		{
+			var map = world.GetResource<Map>();
+			var grid = map.Grid;
 
-            var eventData = world.Entity(eventEntityId).Get<HighlightLocationEvent>();
-            
-            var locEntity = map.Locations.Get(eventData.Coords.Cube);
-            ref var unit = ref locEntity.Get<HasUnit>();
-            var unitEntity = unit.Entity;
-            ref var side = ref unitEntity.Get<Side>();
-            ref var attacks = ref unitEntity.Get<Attacks>();
-            
-            var terrainHighlighter = world.GetResource<TerrainHighlighter>();
-            terrainHighlighter.Clear();
-            
-            var maxAttackRange = attacks.GetMaxAttackRange();
+			var eventData = world.Entity(eventEntityId).Get<HighlightLocationEvent>();
+			
+			var locEntity = map.Locations.Get(eventData.Coords.Cube);
+			ref var unit = ref locEntity.Get<HasUnit>();
+			var unitEntity = unit.Entity;
+			ref var side = ref unitEntity.Get<Side>();
+			ref var attacks = ref unitEntity.Get<Attacks>();
+			
+			var terrainHighlighter = world.GetResource<TerrainHighlighter>();
+			terrainHighlighter.Clear();
+			
+			var maxAttackRange = attacks.GetMaxAttackRange();
+			var cellsInAttackRange = Hex.GetCellsInRange(eventData.Coords.Cube, maxAttackRange);
 
-            var cellsInMoveRange = Hex.GetCellsInRange(eventData.Coords.Cube, eventData.Range);
-            var cellsInAttackRange = Hex.GetCellsInRange(eventData.Coords.Cube, maxAttackRange);
+			foreach (var cCell in cellsInAttackRange)
+			{
+				var nCoords = Coords.FromCube(cCell);
+				if (!grid.IsCoordsInGrid(nCoords))
+				{
+					continue;
+				}
 
-            foreach (var cCell in cellsInAttackRange)
-            {
-                var nCoords = Coords.FromCube(cCell);
-                if (!grid.IsCoordsInGrid(nCoords))
-                {
-                    continue;
-                }
+				var nLocEntity = map.Locations.Dict[nCoords.Cube];
 
-                var nLocEntity = map.Locations.Dict[nCoords.Cube];
+				var attackRange = map.GetEffectiveAttackDistance(eventData.Coords, nCoords);
+				var attackerBonusAttackRange = map.GetBonusAttackRange(eventData.Coords, nCoords);
 
-                var attackRange = map.GetEffectiveAttackDistance(eventData.Coords, nCoords);
-                var attackerBonusAttackRange = map.GetBonusAttackRange(eventData.Coords, nCoords);
+				var attack = attacks.GetUsableAttack(attackRange, attackerBonusAttackRange);
 
-                var attack = attacks.GetUsableAttack(attackRange, attackerBonusAttackRange);
+				ref var nElevation = ref nLocEntity.Get<Elevation>();
+				
+				var position = nCoords.World;
+				position.y = nElevation.Height + 0.1f;
+				
+				var hasUnit = nLocEntity.Has<HasUnit>();
 
-                ref var nElevation = ref nLocEntity.Get<Elevation>();
-                
-                var position = nCoords.World;
-                position.y = nElevation.Height + 0.1f;
-                
-                var hasUnit = nLocEntity.Has<HasUnit>();
+				if (hasUnit)
+				{
+					if (nLocEntity.Get<HasUnit>().Entity.Get<Side>().Value == side.Value)
+					{
+						continue;
+					}
+				}
 
-                if (hasUnit)
-                {
-                    if (nLocEntity.Get<HasUnit>().Entity.Get<Side>().Value == side.Value)
-                    {
-                        continue;
-                    }
-                }
+				if (attack.IsAlive())
+				{
+					if (attackRange > 1)
+					{
+						//terrainHighlighter.PlaceHighlight(position, new Color("881111"), 0.6f);
+					}
+					else
+					{
+						//terrainHighlighter.PlaceHighlight(position, new Color("774411"), 0.6f);
+					}
+				}
+			}
 
-                if (attack.IsAlive())
-                {
-                    if (attackRange > 1)
-                    {
-                        terrainHighlighter.PlaceHighlight(position, new Color("881111"), 0.6f);
-                    }
-                    else
-                    {
-                        terrainHighlighter.PlaceHighlight(position, new Color("774411"), 0.6f);
-                    }
-                }
-            }
+			Vector3[] cellsInMoveRange = Hex.GetCellsInRange(eventData.Coords.Cube, eventData.Range);
+			List<Coords> filteredList = new List<Coords>();
 
-            foreach (var cCell in cellsInMoveRange)
-            {
-                var nCoords = Coords.FromCube(cCell);
-                if (!grid.IsCoordsInGrid(nCoords))
-                {
-                    continue;
-                }
+			foreach (Vector3 cCell in cellsInMoveRange)
+			{
+				Coords nCoords = Coords.FromCube(cCell);
+				if (!grid.IsCoordsInGrid(nCoords))
+				{
+					continue;
+				}
 
-                var nLocEntity = map.Locations.Dict[nCoords.Cube];
+				var nLocEntity = map.Locations.Dict[cCell];
 
-                if (nLocEntity.Get<Distance>().Value > eventData.Range)
-                {
-                    continue;
-                }
+				if (nLocEntity.Get<Distance>().Value > eventData.Range)
+				{
+					continue;
+				}
 
-                ref var nElevation = ref nLocEntity.Get<Elevation>();
-                
-                var position = nCoords.World;
-                position.y = nElevation.Height + 0.1f;
-                
-                var hasUnit = nLocEntity.Has<HasUnit>();
+				ref var nElevation = ref nLocEntity.Get<Elevation>();
+				var hasUnit = nLocEntity.Has<HasUnit>();
 
-                if (hasUnit)
-                {
-                    if (nLocEntity.Get<HasUnit>().Entity.Get<Side>().Value == side.Value)
-                    continue;
-                }
+				if (hasUnit)
+				{
+					if (nLocEntity.Get<HasUnit>().Entity.Get<Side>().Value == side.Value)
+					{
+						continue;
+					}
+				}
 
-                if (hasUnit)
-                {
-                    continue;
-                }
-                
-                terrainHighlighter.PlaceHighlight(position, new Color("111188"), 0.8f);
-            }
-        }
-    }
+				if (hasUnit)
+				{
+					continue;
+				}
+
+				Vector3 position = nCoords.World;
+				position.y = nElevation.Height + 0.1f;
+				bool isEdge = false;
+				Vector3[] Neighbors = Hex.GetNeighbors(nCoords.Cube);
+				GD.Print(nCoords.ToString());
+				for (Direction direction = Direction.NE; direction <= Direction.SE; direction++) {
+					Vector3 cNeighbor = Neighbors[(int)direction];
+					Coords nNeighbor = Coords.FromCube(cNeighbor);
+					bool validCoord = grid.IsCoordsInGrid(nNeighbor);
+					bool inRange = map.Locations.Dict[cNeighbor].Get<Distance>().Value <= eventData.Range;
+					if (!validCoord || !inRange) {
+						isEdge = true;
+						terrainHighlighter.PlaceBorder(position, new Color("111188"), _rotations[direction]);
+						GD.Print($"- {direction}");
+					}
+				}
+				if (!isEdge) {
+					continue;
+				}
+			}
+		}
+	}
 }
