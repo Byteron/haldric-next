7dffbe6261f8890a726ec9c4bd675aec71f9c44f
Aaron Winter <winter.aaron93@gmail.com>
2021-11-12 08:53:03 +0100

    match making + joining match

--- src/nodes/menu/MatchView.cs
+++ src/nodes/menu/MatchView.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Bitron.Ecs;
 using Godot;
 using Nakama;
@@ -5,25 +6,28 @@ public partial class MatchView : Control
 {
     Label _label;
 
+    ISocket _socket;
     IMatchmakerTicket _ticket;
     IMatch _match;
+    IUserPresence _localPlayer;
 
     public override void _Ready()
     {
         _label = GetNode<Label>("PanelContainer/CenterContainer/VBoxContainer/Label");
 
-        Network.Instance.Socket.ReceivedMatchmakerMatched += OnReceivedMatchmakerMatched;
+        _socket = Network.Instance.Socket;
+
+        _socket.ReceivedMatchmakerMatched += OnReceivedMatchmakerMatched;
     }
 
     public override void _ExitTree()
     {
-        Network.Instance.Socket.ReceivedMatchmakerMatched -= OnReceivedMatchmakerMatched;
+        _socket.ReceivedMatchmakerMatched -= OnReceivedMatchmakerMatched;
     }
 
     void OnReceivedMatchmakerMatched(IMatchmakerMatched matched)
     {
-        var opponents = string.Join(",\n  ", matched.Users);
-        _label.Text = "Matched opponents: " + opponents;
+        CreateMatchWith(matched);
     }
 
     void OnCreateButtonPressed()
@@ -33,22 +37,20 @@ public partial class MatchView : Control
 
     void OnJoinButtonPressed()
     {
-        JoinMatch();
+        JoinMatchmaking();
     }
 
     void OnCancelButtonPressed()
     {
-        var socket = Network.Instance.Socket;
-
         if (_ticket != null)
         {
-            socket.RemoveMatchmakerAsync(_ticket);
+            _socket.RemoveMatchmakerAsync(_ticket);
             _ticket = null;
             _label.Text = "Left Matchmaking";
         }
         else if (_match != null)
         {
-            socket.LeaveMatchAsync(_match);
+            _socket.LeaveMatchAsync(_match);
             _match = null;
             _label.Text = "Closed Match";
         }
@@ -58,32 +60,37 @@ public partial class MatchView : Control
         }
     }
 
-    private async void CreateMatch()
+    private async void CreateMatchWith(IMatchmakerMatched matched)
     {
-        if (_ticket != null || _match != null)
-        {
-            return;
-        }
-
-        var socket = Network.Instance.Socket;
-        _match = await socket.CreateMatchAsync();
-        _label.Text = "Match Created: " + _match.Id;
+        _localPlayer = matched.Self.Presence;
+        _match = await _socket.JoinMatchAsync(matched);
+        _label.Text = _localPlayer.Username + " Joined Match: " + _match.Id;
     }
 
-    private async void JoinMatch()
+    private async Task CreateMatch()
     {
         if (_ticket != null || _match != null)
         {
             return;
         }
 
-        var socket = Network.Instance.Socket;
+        _match = await _socket.CreateMatchAsync();
+        _label.Text = "Match Created: " + _match.Id;
+
+    }
+
+    private async void JoinMatchmaking()
+    {
+        if (_ticket != null || _match != null)
+        {
+            return;
+        }
 
         var query = "*";
         var minCount = 2;
         var maxCount = 2;
 
-        _ticket = await socket.AddMatchmakerAsync(query, minCount, maxCount);
+        _ticket = await _socket.AddMatchmakerAsync(query, minCount, maxCount);
         _label.Text = "Looking for Match...";
     }
 }
