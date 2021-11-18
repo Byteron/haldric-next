84cef35c10b3ada73e87e303c2eecd9e461fe0a1
Lordbob <4284022+Lordbob@users.noreply.github.com>
2021-11-18 13:48:39 +0100

    Updated textures
    Reverted to png textures due to jpg artifacts
    Added stone terrain textures

--- data/TerrainScript.cs
+++ data/TerrainScript.cs
@@ -12,66 +12,66 @@ public class TerrainScript : TerrainLoader
 		NewBase("Sb", new List<TerrainType>() { TerrainType.Rough }, -0.4f);		// sand beach
 		NewBase("Ms", new List<TerrainType>() { TerrainType.Rocky });		// mountains simple
 
-		NewShallowWater("Ws", new List<TerrainType>() { TerrainType.ShallowWaters });
-		NewDeepWater("Wo", new List<TerrainType>() { TerrainType.DeepWaters });
+		NewShallowWater("Ws", new List<TerrainType>() { TerrainType.ShallowWaters });		// shallow water
+		NewDeepWater("Wo", new List<TerrainType>() { TerrainType.DeepWaters });		// deep water
 
-		NewOverlay("Ff", new List<TerrainType>() { TerrainType.Forested });
+		NewOverlay("Ff", new List<TerrainType>() { TerrainType.Forested });		// forest
 
-		NewKeep("Kh", new List<TerrainType>() { TerrainType.Fortified });
-		NewCastle("Ch", new List<TerrainType>() { TerrainType.Fortified });
+		NewKeep("Kh", new List<TerrainType>() { TerrainType.Fortified });		// keep human
+		NewCastle("Ch", new List<TerrainType>() { TerrainType.Fortified });		// castle human
 
-		NewHouses("Vh", new List<TerrainType>() { TerrainType.Settled });
-		NewVillage("VHh", new List<TerrainType>() { TerrainType.Settled, TerrainType.Fortified });
+		NewHouses("Vh", new List<TerrainType>() { TerrainType.Settled });		// village human
+		NewVillage("VHh", new List<TerrainType>() { TerrainType.Settled, TerrainType.Fortified });		// village hall human
 
 //      Grass  
-		AddTerrainTexture("Gg", "assets/graphics/images/grass_basecolor.jpg");
-		AddTerrainNormalTexture("Gg", "assets/graphics/images/grass_normal.jpg");
-		AddTerrainRoughnessTexture("Gg", "assets/graphics/images/grass_roughness.jpg");
+		AddTerrainTexture("Gg", "assets/graphics/images/grass_basecolor.png");
+		AddTerrainNormalTexture("Gg", "assets/graphics/images/grass_normal.png");
+		AddTerrainRoughnessTexture("Gg", "assets/graphics/images/grass_roughness.png");
 
 //		Sand desert
-		AddTerrainTexture("Sd", "assets/graphics/images/sand_dunes_basecolor.jpg");
-		AddTerrainNormalTexture("Sd", "assets/graphics/images/sand_dunes_normal.jpg");
-		AddTerrainRoughnessTexture("Sd", "assets/graphics/images/sand_dunes_roughness.jpg");
+		AddTerrainTexture("Sd", "assets/graphics/images/sand_dunes_basecolor.png");
+		AddTerrainNormalTexture("Sd", "assets/graphics/images/sand_dunes_normal.png");
+		AddTerrainRoughnessTexture("Sd", "assets/graphics/images/sand_dunes_roughness.png");
 
 //		Sand beach
-		AddTerrainTexture("Sb", "assets/graphics/images/sand_beach_basecolor.jpg");
-		AddTerrainNormalTexture("Sb", "assets/graphics/images/sand_beach_normal.jpg");
-		AddTerrainRoughnessTexture("Sb", "assets/graphics/images/sand_beach_roughness.jpg");
+		AddTerrainTexture("Sb", "assets/graphics/images/sand_beach_basecolor.png");
+		AddTerrainNormalTexture("Sb", "assets/graphics/images/sand_beach_normal.png");
+		AddTerrainRoughnessTexture("Sb", "assets/graphics/images/sand_beach_roughness.png");
 
 //		Sand mud
-		AddTerrainTexture("Sm", "assets/graphics/images/mud_basecolor.jpg");
-		AddTerrainNormalTexture("Sm", "assets/graphics/images/mud_normal.jpg");
-		AddTerrainRoughnessTexture("Sm", "assets/graphics/images/mud_roughness.jpg");
+		AddTerrainTexture("Sm", "assets/graphics/images/mud_basecolor.png");
+		AddTerrainNormalTexture("Sm", "assets/graphics/images/mud_normal.png");
+		AddTerrainRoughnessTexture("Sm", "assets/graphics/images/mud_roughness.png");
 
-//		Mountains rocky
-		AddTerrainTexture("Ms", "assets/graphics/images/stone_basecolor.jpg");
-		AddTerrainNormalTexture("Ms", "assets/graphics/images/stone_normal.jpg");
-		AddTerrainRoughnessTexture("Ms", "assets/graphics/images/stone_roughness.jpg");
+//		Mountains simple
+		AddTerrainTexture("Ms", "assets/graphics/images/stone_basecolor.png");
+		AddTerrainNormalTexture("Ms", "assets/graphics/images/stone_normal.png");
+		AddTerrainRoughnessTexture("Ms", "assets/graphics/images/stone_roughness.png");
 		
 //		Road dirt
-		AddTerrainTexture("Rd", "assets/graphics/images/dirt_basecolor.jpg");
-		AddTerrainNormalTexture("Rd", "assets/graphics/images/dirt_normal.jpg");
-		AddTerrainRoughnessTexture("Rd", "assets/graphics/images/dirt_roughness.jpg");
+		AddTerrainTexture("Rd", "assets/graphics/images/dirt_basecolor.png");
+		AddTerrainNormalTexture("Rd", "assets/graphics/images/dirt_normal.png");
+		AddTerrainRoughnessTexture("Rd", "assets/graphics/images/dirt_roughness.png");
 
 //		Castle human
-		AddTerrainTexture("Ch", "assets/graphics/images/dirt_castle_basecolor.jpg");
-		AddTerrainNormalTexture("Ch", "assets/graphics/images/dirt_castle_normal.jpg");
-		AddTerrainRoughnessTexture("Ch", "assets/graphics/images/dirt_castle_roughness.jpg");
+		AddTerrainTexture("Ch", "assets/graphics/images/dirt_castle_basecolor.png");
+		AddTerrainNormalTexture("Ch", "assets/graphics/images/dirt_castle_normal.png");
+		AddTerrainRoughnessTexture("Ch", "assets/graphics/images/dirt_castle_roughness.png");
 
 //		Keep human
-		AddTerrainTexture("Kh", "assets/graphics/images/dirt_castle_basecolor.jpg");
-		AddTerrainNormalTexture("Kh", "assets/graphics/images/dirt_castle_normal.jpg");
-		AddTerrainRoughnessTexture("Kh", "assets/graphics/images/dirt_castle_roughness.jpg");
+		AddTerrainTexture("Kh", "assets/graphics/images/dirt_castle_basecolor.png");
+		AddTerrainNormalTexture("Kh", "assets/graphics/images/dirt_castle_normal.png");
+		AddTerrainRoughnessTexture("Kh", "assets/graphics/images/dirt_castle_roughness.png");
 
 //		Water shallow
-		AddTerrainTexture("Ws", "assets/graphics/images/mud_basecolor.jpg");
-		AddTerrainNormalTexture("Ws", "assets/graphics/images/mud_normal.jpg");
-		AddTerrainRoughnessTexture("Ws", "assets/graphics/images/mud_roughness.jpg");
+		AddTerrainTexture("Ws", "assets/graphics/images/mud_basecolor.png");
+		AddTerrainNormalTexture("Ws", "assets/graphics/images/mud_normal.png");
+		AddTerrainRoughnessTexture("Ws", "assets/graphics/images/mud_roughness.png");
 		
 //		Water deep
-		AddTerrainTexture("Wo", "assets/graphics/images/mud_basecolor.jpg");
-		AddTerrainNormalTexture("Wo", "assets/graphics/images/mud_normal.jpg");
-		AddTerrainRoughnessTexture("Wo", "assets/graphics/images/mud_roughness.jpg");
+		AddTerrainTexture("Wo", "assets/graphics/images/mud_basecolor.png");
+		AddTerrainNormalTexture("Wo", "assets/graphics/images/mud_normal.png");
+		AddTerrainRoughnessTexture("Wo", "assets/graphics/images/mud_roughness.png");
 		
 		// AddKeepPlateauGraphic("Kh", "assets/graphics/models/keep_plateau.tres", new Godot.Vector3(0f, 1.5f, 0f));
 		AddWallSegmentGraphic("Kh", "assets/graphics/models/keep_wall.tres");
