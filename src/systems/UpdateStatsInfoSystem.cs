ff698680fd04208642bb940385e1823a361f7d16
Aaron Winter <winter.aaron93@gmail.com>
2022-05-27 16:05:13 +0200

    tweaks

--- src/systems/UpdateStatsInfoSystem.cs
+++ src/systems/UpdateStatsInfoSystem.cs
@@ -1,6 +1,4 @@
-using Godot;
 using RelEcs;
-using RelEcs.Godot;
 
 public class UpdateStatsInfoSystem : ISystem
 {
@@ -9,7 +7,8 @@ public class UpdateStatsInfoSystem : ISystem
         if (!commands.TryGetElement<DebugPanel>(out var panel)) return;
         if (!commands.TryGetElement<WorldInfo>(out var info)) return;
 
-        var s = $"Entities: {info.EntityCount}\nArchetypes: {info.ArchetypeCount}\nSystems: {info.SystemCount}";
+        var s = $"Entities: {info.EntityCount}\nElements: {info.ElementCount}\nArchetypes: {info.ArchetypeCount}";
+        s += $"\nSystems: {info.SystemCount}\nQueries: {info.CachedQueryCount}";
         
         foreach (var (type, time) in info.SystemExecutionTimes)
         {
